6d8a23329960f069d124d4229ccc0ddde95213ba
Rich <4301176+rich-chang@users.noreply.github.com>
2021-03-10 11:31:13 +0800

    Update main.dart
    Add dice images to stateless container

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blueGrey.shade400,
         ),
         body: DicePage(),
       ),
@@ -18,6 +18,21 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Image(
+                image: AssetImage('images/dice1.png')
+            ),
+          ),
+          Expanded(
+              child: Image(
+                  image: AssetImage('images/dice4.png')
+              ),
+          ),
+        ],
+      ),
+    );
   }
 }
